c47be8dec8d16c9fb21f32840ad69e5142a41ae3
RThemian <tomas.cservenak@gmail.com>
2022-04-09 09:22:48 -0600

    Updated with button and functional API

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>
+      This sucks! My money sucks!
+      </h1>
+      <div>
+        <form>Input Dollars here
+          <input
+            type = "checkbox"/>
+            <input type="time"/>
+            <input type="url"/>
+            <input type="week"/>
+
+        </form>
+        </div>
+        <div>
+        <p>Choose your favorite Web language:</p>
+
+<form>
+  <input type="radio" id="html" name="fav_language" value="HTML">
+  <label for="html">HTML</label><br>
+  <input type="radio" id="css" name="fav_language" value="CSS">
+  <label for="css">CSS</label><br>
+  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+  <label for="javascript">JavaScript</label>
+</form>
+        </div>
+      </div>
+    
   );
 }
 
